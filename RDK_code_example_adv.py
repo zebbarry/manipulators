84a4736609d4460f10f1b5746c97f7195b4b0e5e
Zeb Barry <zba24@uclive.ac.nz>
2020-09-14 15:04:40 +1200

    init

---
+++ RDK_code_example_adv.py
@@ -0,0 +1,64 @@
+# A more advanced example to get you moving with the RoboDK python API
+# Note, as there are many solutions for a given pose, sometimes when
+# running this, the robot may choose a weird pose that then doesn't allow
+# the subsequent motion (due to being near a singularity etc). If this occurs, 
+# just manually reset the robot startingposition to somewhere else and try again
+# C Pretty, Sept 2019
+
+import robolink as rl    # RoboDK API
+import robodk as rdk     # Robot toolbox
+import numpy as np
+
+RDK = rl.Robolink()
+
+robot = RDK.Item('UR5')
+world_frame = RDK.Item('UR5 Base')
+target = RDK.Item('Home')   # existing target in station
+robot.setPoseFrame(world_frame)
+robot.setPoseTool(robot.PoseTool())
+
+#Existing subprograms
+grinder_tool_attach = RDK.Item('Grinder Tool Attach')
+grinder_tool_detach = RDK.Item('Grinder Tool Detach')
+
+# Use the RDK Matrix object from to hold pose (its an HT)
+T_home = rdk.Mat([[     0.000000,     0.000000,     1.000000,   523.370000 ],
+     [-1.000000,     0.000000,     0.000000,  -109.000000 ],
+     [-0.000000,    -1.000000,     0.000000,   607.850000 ],
+      [0.000000,     0.000000,     0.000000,     1.000000 ]])
+
+
+T_intermediatepoint = rdk.Mat([[    -0.642784,    -0.766037,    -0.004005,  -501.690000],
+    [ -0.766007,     0.642795,    -0.006929,  -144.638000],
+    [  0.007883,    -0.001386,    -0.999968,   498.577000],
+    [  0.000000,     0.000000,     0.000000,     1.000000]])
+
+
+T_grinderapproach = rdk.Mat([[     0.173648,    -0.984800,    -0.004000,  -502.103741],
+    [ -0.984789,    -0.173618,    -0.006928,  -145.353888 ],
+    [  0.006128,     0.005142,    -0.999968,   535.250260 ],
+    [  0.000000,     0.000000,     0.000000,     1.000000 ]])
+
+
+robot.MoveJ(T_home, blocking=True)
+robot.MoveJ(T_intermediatepoint, blocking=True)
+robot.MoveL(T_grinderapproach, blocking=True)
+
+grinder_tool_attach.RunCode(grinder_tool_attach) # call subprogram
+rdk.pause(5)  # to allow subprogram to complete
+grinder_tool_detach.RunCode(grinder_tool_detach) # call subfunction
+
+# The following pause is very important - if it is not present, or long enough
+# the frame reset below it occurs before the subprogram completes and this
+# causes problems...
+rdk.pause(5)    
+# Note, the subfunctions change the reference frame, so you need to change it back
+# after calling them
+robot.setPoseFrame(world_frame)
+# you may also need to reset the toolframe
+robot.setPoseTool(robot.PoseTool())
+
+# and... move home to an existing target
+robot.MoveJ(target)
+
+
