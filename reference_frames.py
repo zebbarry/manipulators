a23f01dd7cba55057e8af86823c661173f1e4b8c
Zeb Barry <zba24@uclive.ac.nz>
2020-09-15 17:24:45 +1200

    Added tcp2tool not finished

--- reference_frames.py
+++ reference_frames.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+ROBOT = 'robot'
+SILVIA2ROBOT = 'SILVIA2ROBOT'
+GRINDER2ROBOT = 'GRINDER2ROBOT'
+CUP2ROBOT = 'CUP2ROBOT'
+TOOLS2ROBOT = 'TOOLS2ROBOT'
+SCRAPER2ROBOT = 'SCRAPER2ROBOT'
+TOOL2TCP = 'TOOL2TCP'
+
 
 class ReferenceFrame:
 
@@ -40,16 +48,16 @@ def plot_scene(frames, test_points):
     plt.ylabel('Y (mm)')
     plt.xlabel('X (mm)')
     plt.axis('equal')
-    plt.plot([frames['robot'].position[0], frames['robot'].position[0] + 50],
-             [frames['robot'].position[1], frames['robot'].position[1]],
+    plt.plot([frames[ROBOT].position[0], frames[ROBOT].position[0] + 50],
+             [frames[ROBOT].position[1], frames[ROBOT].position[1]],
              '-r')
-    plt.plot([frames['robot'].position[0], frames['robot'].position[0]],
-             [frames['robot'].position[1], frames['robot'].position[1] + 50],
+    plt.plot([frames[ROBOT].position[0], frames[ROBOT].position[0]],
+             [frames[ROBOT].position[1], frames[ROBOT].position[1] + 50],
              '-g')
 
     scale = 50
     for name, frame in frames.items():
-        if name != 'robot':
+        if name != ROBOT:
             rotation = frame.rotation
             plt.plot([frame.position[0], frame.position[0] + scale * rotation[0][0]],
                      [frame.position[1], frame.position[1] + scale * rotation[1][0]],
@@ -59,24 +67,24 @@ def plot_scene(frames, test_points):
                      '-g')
 
     # Silvia
-    plt.plot([frames['silvia'].ref_point[0]],
-             [frames['silvia'].ref_point[1]],
+    plt.plot([frames[SILVIA2ROBOT].ref_point[0]],
+             [frames[SILVIA2ROBOT].ref_point[1]],
              'ob')
     plt.plot([silvia_robot_point[0]],
              [silvia_robot_point[1]],
              '+', color='orange')
 
     # Grinder
-    plt.plot([frames['grinder'].ref_point[0]],
-             [frames['grinder'].ref_point[1]],
+    plt.plot([frames[GRINDER2ROBOT].ref_point[0]],
+             [frames[GRINDER2ROBOT].ref_point[1]],
              'ob')
     plt.plot([grinder_robot_point[0]],
              [grinder_robot_point[1]],
              '+', color='orange')
 
     # Scraper
-    plt.plot([frames['scraper'].ref_point[0]],
-             [frames['scraper'].ref_point[1]],
+    plt.plot([frames[SCRAPER2ROBOT].ref_point[0]],
+             [frames[SCRAPER2ROBOT].ref_point[1]],
              'ob')
     plt.plot([scraper_robot_point[0]],
              [scraper_robot_point[1]],
@@ -85,24 +93,29 @@ def plot_scene(frames, test_points):
 
 
 def calc_reference_frames(frames):
-    angle = frames['silvia'].calc_angle()
-    frames['silvia'].rotation = np.array([[-np.sin(angle), -np.cos(angle), 0],
+    angle = frames[SILVIA2ROBOT].calc_angle()
+    frames[SILVIA2ROBOT].rotation = np.array([[-np.sin(angle), -np.cos(angle), 0],
                                           [np.cos(angle), -np.sin(angle), 0],
                                           [0, 0, 1]])
 
-    angle = frames['grinder'].calc_angle()
-    frames['grinder'].rotation = np.array([[-np.cos(angle), np.sin(angle), 0],
+    angle = frames[GRINDER2ROBOT].calc_angle()
+    frames[GRINDER2ROBOT].rotation = np.array([[-np.cos(angle), np.sin(angle), 0],
                                            [-np.sin(angle), -np.cos(angle), 0],
                                            [0, 0, 1]])
 
-    angle = frames['scraper'].calc_angle()
-    frames['scraper'].rotation = np.array([[np.cos(angle), -np.sin(angle), 0],
+    angle = frames[SCRAPER2ROBOT].calc_angle()
+    frames[SCRAPER2ROBOT].rotation = np.array([[np.cos(angle), -np.sin(angle), 0],
                                            [np.sin(angle), -np.cos(angle), 0],
                                            [0, 0, 1]])
 
-    frames['cup'].rotation = np.array([[-1, 0, 0],
-                                       [0, -1, 0],
-                                       [0, 0, 1]])
+    frames[CUP2ROBOT].rotation = np.array([[-1, 0, 0],
+                                           [0, -1, 0],
+                                           [0, 0, 1]])
+
+    angle = 50
+    frames[TOOL2TCP].rotation = np.array([[0, 0, 0],
+                                          [0, 0, 0],
+                                          [0, 0, -1]])
     for name, frame in frames.items():
         frame.calc_transform()
     return frames
