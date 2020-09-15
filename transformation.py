98cea09f3cb9329816ff2ee51ee346c1810d5598
Zeb Barry <zba24@uclive.ac.nz>
2020-09-15 17:27:18 +1200

    Merged

--- transformation.py
+++ transformation.py
@@ -3,6 +3,59 @@ import matplotlib.pyplot as plt
 from reference_frames import *
 from robolink import *    # RoboDK API
 from robodk import *      # Robot toolbox
+import matplotlib as plt
+
+
+def plot_scene(frames, test_points):
+    silvia_robot_point, grinder_robot_point, scraper_robot_point = test_points
+
+    # Robot
+    plt.figure()
+    plt.ylabel('Y (mm)')
+    plt.xlabel('X (mm)')
+    plt.axis('equal')
+    plt.plot([frames[ROBOT].position[0], frames[ROBOT].position[0] + 50],
+             [frames[ROBOT].position[1], frames[ROBOT].position[1]],
+             '-r')
+    plt.plot([frames[ROBOT].position[0], frames[ROBOT].position[0]],
+             [frames[ROBOT].position[1], frames[ROBOT].position[1] + 50],
+             '-g')
+
+    scale = 50
+    for name, frame in frames.items():
+        if name != ROBOT:
+            rotation = frame.rotation
+            plt.plot([frame.position[0], frame.position[0] + scale * rotation[0][0]],
+                     [frame.position[1], frame.position[1] + scale * rotation[1][0]],
+                     '-r')
+            plt.plot([frame.position[0], frame.position[0] + scale * rotation[0][1]],
+                     [frame.position[1], frame.position[1] + scale * rotation[1][1]],
+                     '-g')
+
+    # Silvia
+    plt.plot([frames[SILVIA2ROBOT].ref_point[0]],
+             [frames[SILVIA2ROBOT].ref_point[1]],
+             'ob')
+    plt.plot([silvia_robot_point[0]],
+             [silvia_robot_point[1]],
+             '+', color='orange')
+
+    # Grinder
+    plt.plot([frames[GRINDER2ROBOT].ref_point[0]],
+             [frames[GRINDER2ROBOT].ref_point[1]],
+             'ob')
+    plt.plot([grinder_robot_point[0]],
+             [grinder_robot_point[1]],
+             '+', color='orange')
+
+    # Scraper
+    plt.plot([frames[SCRAPER2ROBOT].ref_point[0]],
+             [frames[SCRAPER2ROBOT].ref_point[1]],
+             'ob')
+    plt.plot([scraper_robot_point[0]],
+             [scraper_robot_point[1]],
+             '+', color='orange')
+    plt.show()
 
 
 def main():
