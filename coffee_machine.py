7dc92ddbcd866003ae1565414b5e5e2fb05341bc
Zeb Barry <zba24@uclive.ac.nz>
2020-09-17 19:59:52 +1200

    Added log

--- coffee_machine.py
+++ coffee_machine.py
@@ -8,34 +8,61 @@ import robodk as rdk  # Robot toolbox
 # import numpy as np
 from reference_frames import *
 
-COMMAND_WAIT = 10
+COMMAND_WAIT = 15
 
 GRINDERFUNC = 'Grinder Tool'
 FILTERFUNC = 'Portafilter Tool'
 CUPFUNC = 'Cup Tool'
 ATTACH = ' Attach'
 DETACH = ' Detach'
+OPEN = ' Open'
+CLOSE = ' Close'
 HOME = 'Home'
 
 
 class CoffeeMachine(object):
 
-    def __init__(self, robot, RDK, frames, joint_angles):
+    def __init__(self, robot, RDK, frames, joint_angles, log_file='~/log.txt'):
         self.robot = robot
         self.frames = frames
         self.joint_angles = joint_angles
         self.functions = {}
         self.RDK = RDK
+        self.log_filename = log_file
+        self.log_file = open(logfile, 'w')
+
+    def log(self, message):
+        self.log_file.write(message)
+
+    def close_log(self):
+        close(self.log_file)
 
     def add_function(self, name):
-        self.functions[name] = RDK.Item(name)
+        self.functions[name] = self.RDK.Item(name)
+        self.log("Function '{}' added".format(name))
 
-    def filtermount(self, pickup=True):
-        self.robot.MoveJ(joint_angles[GLOBAL + FILTERMOUNT])
-        self.robot.MoveJ(frames[GLOBAL + FILTERMOUNT])
-        name = FILTERFUNC + (ATTACH if pickup else DETACH)
-        self.functions[name].RunCode(name)
-        rdk.pause(COMMAND_WAIT)
+    def MoveJ(self, matrix):
+        self.robot.MoveJ(matrix)
+        self.log("Moving to new position")
+        self.log(matrix)
+
+    def tool_mount(self, name, pickup=True):
+        if name == GRINDER:
+            mount = GRINDERMOUNT
+            func = GRINDERFUNC
+        elif name == FILTER:
+            mount = FILTERMOUNT
+            func = FILTERFUNC
+        elif name == CUP:
+            mount = CUPMOUNT
+            func = CUPFUNC
+
+        self.robot.MoveJ(self.joint_angles[GLOBAL + mount])
+        self.robot.MoveJ(self.frames[GLOBAL + mount])
+        operation = ATTACH if pickup else DETACH
+        name = func + operation
+        self.functions[name].RunCode(self.functions[name], True)
+        self.log("Tool Mount Operation - Tool: {}, Operation: {}".format(name, operation))
 
 
 def main():
@@ -64,25 +91,37 @@ def main():
     cup_tool_attach = RDK.Item('Cup Tool Attach')
     cup_tool_detach = RDK.Item('Cup Tool Detach')
 
+    machine.add_function(GRINDERFUNC+ATTACH)
+    machine.add_function(GRINDERFUNC+DETACH)
+    machine.add_function(FILTERFUNC+ATTACH)
+    machine.add_function(FILTERFUNC+DETACH)
+    machine.add_function(CUPFUNC+ATTACH)
+    machine.add_function(CUPFUNC+DETACH)
+    machine.add_function(CUPFUNC+OPEN)
+    machine.add_function(CUPFUNC+CLOSE)
+
     # Pickup filter
     robot.MoveJ(home)
-    # robot.MoveJ(joint_angles[GLOBAL + FILTERMOUNT])
-    # robot.MoveJ(frames[GLOBAL + FILTERMOUNT])
-    # filter_tool_attach.RunCode(filter_tool_attach)
-    # # filter_tool_detach.RunCode(filter_tool_detach)
-    # rdk.pause(COMMAND_WAIT)
-    machine.filtermount(True)
+    machine.tool_mount(GRINDER, True)
+    machine.tool_mount(GRINDER, False)
     robot.MoveJ(home)
-    machine.filtermount(False)
+    machine.tool_mount(FILTER, True)
+    machine.tool_mount(FILTER, False)
+    robot.MoveJ(home)
+    machine.tool_mount(CUP, True)
+    machine.tool_mount(CUP, False)
+    robot.MoveJ(home)
 
     # Move to ball
-    frames[BALL+TOOL] = frames[TOOL+BALL].inv()
-    frames[TOOL+TCP] = frames[TCP+TOOL].inv()
-    frames[FILTER+TOOL] = frames[TOOL+FILTER].inv()
-    global2end = frames[GLOBAL+GRINDER] * frames[GRINDER+BALL] * frames[BALL+TOOL] * frames[TOOL+TCP]
-    # robot.MoveJ(global2end)
-    # rdk.pause(COMMAND_WAIT)
+    frames[BALL + TOOL] = frames[TOOL + BALL].inv()
+    frames[TOOL + TCP] = frames[TCP + TOOL].inv()
+    frames[FILTER + TOOL] = frames[TOOL + FILTER].inv()
+    global2end = frames[GLOBAL + GRINDER] * frames[GRINDER + BALL] * frames[BALL + TOOL] * frames[TOOL + TCP]
+    robot.MoveJ(machine.joint_angles[GLOBAL+'filterinsert'])
+    robot.MoveJ(global2end)
     # robot.MoveJ(home)
+
+    machine.close_log()
 
 
 main()
