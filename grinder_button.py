70613d5996d9f596f4cbacdcc44205dd328fb30e
jhz11 <jhz11@uclive.ac.nz>
2020-09-22 15:53:28 +1200

    added shit

--- grinder_button.py
+++ grinder_button.py
@@ -10,7 +10,7 @@ import robodk as rdk  # Robot toolbox
 # import numpy as np
 from reference_frames import *
 
-COMMAND_WAIT = 10
+COMMAND_WAIT = 2
 
 def move_filter(frames):
     pass
@@ -37,13 +37,18 @@ cup_tool_attach = RDK.Item('Cup Tool Attach')
 cup_tool_detach = RDK.Item('Cup Tool Detach')
 
 # Pickup filter
-robot.MoveJ(frames[GLOBAL+FILTERMOUNT])
-filter_tool_attach.RunCode(filter_tool_attach)
+robot.MoveJ(frames[GLOBAL+GRINDERMOUNT])
+grinder_tool_attach.RunCode(grinder_tool_attach)
 rdk.pause(COMMAND_WAIT)
 robot.MoveJ(home)
+rdk.pause(COMMAND_WAIT)
 
 # Move to ball
-frames[BALL+TOOL] = frames[TOOL+BALL].inv()
-# frames[TOOL+TCP] = frames[TCP+TOOL].inv()
-global2end = frames[GLOBAL+GRINDER] * frames[GRINDER+BALL] * frames[BALL+TOOL] #* frames[TOOL+TCP]
+#frames[BALL+TOOL] = frames[TOOL+BALL].inv()
+frames[TOOL+TCP] = frames[TCP+TOOL].inv()
+#global2end = frames[GLOBAL+GRINDER] * frames[GRINDER+BALL] * frames[BALL+TOOL] #* frames[TOOL+TCP]
+global2end = frames[GLOBAL+SILVIA] * frames[SILVIA+SILVIAPOWERBUTTON] * frames[TOOL+TCP]
 robot.MoveJ(global2end)
+
+robot.MoveJ(frames[GLOBAL+GRINDERMOUNT])
+grinder_tool_detach.RunCode(grinder_tool_detach)
